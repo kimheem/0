70655c4d6b8d92cd7f891012f3323687a28b1e58
kimheem <107026489+kimheem@users.noreply.github.com>
2022-06-19 02:59:41 +0900

    Add files via upload

---
+++ colors.js
@@ -0,0 +1,35 @@
+var Body = {
+    setColor:function(color){
+        document.querySelector('body').style.color = color;
+    },
+    setBackgroundColor:function(color){
+        document.querySelector('body').style.backgroundColor = color;
+    }
+}
+var Link = {
+    setcolor:function(color){
+        var alist = document.querySelectorAll('a');
+        var i = 0
+        while(i < alist.length){
+        alist[i].style.color = color;
+        i = i + 1;
+    }
+    }
+}
+
+function nightdayhandler(self){
+    var target = document.querySelector('body');
+    if (self.value === 'Night'){
+        Body.setBackgroundColor('black');
+        Body.setColor('white');
+        self.value = 'Day';
+
+        Link.setcolor('powderblue');
+    } else {
+        Body.setBackgroundColor('white');
+        Body.setColor('black');
+        self.value = 'Night';
+
+        Link.setcolor('black');
+    }
+}
